8a94c16334da0bda6c3701996efc830b2edb6704
Arshitraj2005 <nigachu789@gmail.com>
2025-09-27 13:55:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import threading
 
 # ==== CONFIG ====
 STREAM_KEY = "cec7-xy4y-9y7e-xk7t-4qxa"        # YouTube stream key
-VIDEO_DRIVE_ID = "1SqqVbApLnkj8rSnmfYBH7Yva90MxhPwa"   # Drive ID of video/GIF
+VIDEO_DRIVE_ID = "1zOqir9W5hYTbHMAAolrs5Dh71XwZHX7l"   # Drive ID of video/GIF
 AUDIO_DRIVE_ID = "1fO8xVEIKALIZAMMYcFEMQK4Rk0cFtBp6"  # Drive ID of audio
 
 VIDEO_FILE = "overlay.mp4"   # Downloaded video/GIF
